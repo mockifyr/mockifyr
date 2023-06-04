3c87c9438e63deba21c9d192f8f2fe15fd0b2dd0
Ömer Çelik <oclk@outlook.com.tr>
2023-06-04 15:45:14 +0300

    feat: Created Auth - register modules

---
+++ Source/Application/Features/Identity/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Application.Features.Identity.Auth.Commands.Register;
+
+public class RegisterCommand : IRequest<bool>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, bool>
+{
+    public async Task<bool> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Source/Application/Features/Identity/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Identity.Auth.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        //RuleFor(v => v.Email)
+        //    .MaximumLength(200)
+        //    .NotEmpty();
+    }
+}
