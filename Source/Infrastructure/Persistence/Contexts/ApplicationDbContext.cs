e0af6a8fbf5b03d53c90befb800b1df988b1e9bf
Ömer Çelik <oclk@outlook.com.tr>
2023-06-04 12:55:46 +0300

    feat: Added db nugets and ApplicationDbContext class

---
+++ Source/Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Contexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Codes for creating models and defining relationships
+    }
+}
--- Source/Presentation/Program.cs
+++ Source/Presentation/Program.cs
@@ -1,4 +1,8 @@
+﻿using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +10,39 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    string connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+    string provider = Environment.GetEnvironmentVariable("DatabaseProvider");
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+    switch (provider)
+    {
+        case "sqlite":
+            options.UseSqlite(connectionString);
+            break;
+        case "postgres":
+            options.UseNpgsql(connectionString);
+            break;
+        case "mssql":
+            options.UseSqlServer(connectionString);
+            break;
+        case "mysql":
+#pragma warning disable CS8604 // Possible null reference argument.
+            options.UseMySQL(connectionString);
+#pragma warning restore CS8604 // Possible null reference argument.
+            break;
+        case "oraclesql":
+            options.UseOracle(connectionString);
+            break;
+        default:
+            options.UseSqlite(connectionString); // Use SQLite by default
+            break;
+    }
+});
 
 var app = builder.Build();
 
